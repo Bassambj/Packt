9b572ff0c33117c83999208628ac9a82b8ddc4a9
Bassambj <bassambj@gmail.com>
2021-11-25 23:22:30 -0300

    Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,33 @@
-fun main() {
+package pack
 
+fun main() {
+    val basic = Basics()
+    basic.welcome()
+    basic.getRange()
 }
 
 class Basics(){
+    val pack = "Packet"
+    var hellow = "Hello $pack"
 
+    fun welcome(){
+        println("$hellow. Welcome!")
+    }
+
+    val aToZ = "a".."z"
+    val oneToTen = 1..10
+    var x: Int = 9
+    val nine = x in oneToTen // in test if nine is in range oneToTen
+
+    fun getRange(){
+        println("Alphabet range: $aToZ")
+        if (nine) (println ("ok, $nine, $x is in $oneToTen"))
+    }
+    /* Ranges:
+    reversed() reversed interval
+    downTo()   decreasing interval
+    rangeTo()  // range to a value
+    */
 }
 
 
